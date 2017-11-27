9489251dc2dd8f3c07730a39f804de04886e5ebb
Henrik Bechmann <henrik@therefore.ca>
2017-11-27 13:57:32 -0500

    further remove obsolete actions

--- js/core/actions/actions.js
+++ js/core/actions/actions.js
@@ -1,83 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const redux_actions_1 = require("redux-actions");
 const react_router_redux_1 = require("react-router-redux");
-exports.ADD_TILE = 'ADD_TILE';
-exports.REMOVE_TILE = 'REMOVE_TILE';
-exports.UPDATE_TILE = 'UPDATE_TILE';
 exports.pushHistory = route => {
     return dispatch => {
         dispatch(react_router_redux_1.routerActions.push(route));
-    };
-};
-exports.LOGIN_REQUEST = 'LOGIN_REQUEST';
-exports.LOGIN_SUCCESS = 'LOGIN_SUCCESS';
-exports.LOGIN_FAILURE = 'LOGIN_FAILURE';
-let requestLogin = redux_actions_1.createAction(exports.LOGIN_REQUEST, creds => {
-    return {
-        creds,
-    };
-});
-let receiveLogin = redux_actions_1.createAction(exports.LOGIN_SUCCESS, user => {
-    return {
-        token: user.token,
-        profile: user.profile,
-    };
-});
-let loginError = redux_actions_1.createAction(exports.LOGIN_FAILURE, (message, data) => {
-    return {
-        message,
-        data,
-    };
-});
-exports.loginUser = (creds, callback) => {
-    let config = {
-        method: 'POST',
-        headers: { "Content-Type": "application/x-www-form-urlencoded", },
-        body: `email=${creds.email}&password=${creds.password}`,
-    };
-    return dispatch => {
-        dispatch(requestLogin(creds));
-        fetch('/api/login/credentials', config)
-            .then(response => {
-            if (response.status >= 500) {
-                throw new Error("Response from server: " +
-                    response.statusText + ' (' +
-                    response.status + ')');
-            }
-            return response.text().then(text => {
-                return { text, response };
-            });
-        })
-            .then(({ text, response }) => {
-            let json, isJson;
-            try {
-                json = JSON.parse(text);
-                isJson = true;
-            }
-            catch (e) {
-                isJson = false;
-            }
-            if (!isJson || !response.ok) {
-                if (isJson) {
-                    dispatch(loginError(json.message, json.data));
-                }
-                else {
-                    dispatch(loginError(text));
-                }
-                callback(false);
-            }
-            else {
-                localStorage.setItem('jsonwebtoken', json.token);
-                dispatch(() => {
-                    dispatch(receiveLogin(json));
-                });
-                callback(true);
-            }
-        })
-            .catch(err => {
-            dispatch(loginError(err.message));
-            callback(false);
-        });
     };
 };
--- ts/core/actions/actions.tsx
+++ ts/core/actions/actions.tsx
@@ -20,116 +20,15 @@ import { getQuery } from '../utilities/utilities'
 //===============================================
 /*------------- tile management -----------*/
 
-// the following three to be implemented
-export const ADD_TILE = 'ADD_TILE'
-export const REMOVE_TILE = 'REMOVE_TILE'
-export const UPDATE_TILE = 'UPDATE_TILE'
-
 export const pushHistory = route => {
     return dispatch => {
         dispatch(routerActions.push(route))
     }
 }
 
-//===============================================
-/*------------- login management -----------*/
-
-export const LOGIN_REQUEST = 'LOGIN_REQUEST'
-export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
-export const LOGIN_FAILURE = 'LOGIN_FAILURE'
-
-let requestLogin = createAction(
-    LOGIN_REQUEST,
-    creds => { 
-        return {
-            creds,
-        }
-    }
-) 
-
-let receiveLogin = createAction(
-    LOGIN_SUCCESS,
-    user => {
-        return {
-            token: user.token,
-            profile: user.profile,
-        }
-    }
-)
-
-let loginError = createAction(
-    LOGIN_FAILURE,
-    (message, data?) => {
-        return {
-            message,
-            data,
-        }
-    }
-)
-
 interface RequestInit {
     method:string,
     headers: any,
     body:string,
-}
-
-
-// call the api
-export const loginUser = (creds, callback) => {
-
-    let config:RequestInit = {
-        method: 'POST',
-        headers: { "Content-Type": "application/x-www-form-urlencoded", },
-        body: `email=${creds.email}&password=${creds.password}`,
-    }
-    return dispatch => {
-        dispatch(requestLogin(creds))
-        fetch('/api/login/credentials', config)
-            .then(response => {
-
-                if (response.status >= 500) {
-                    throw new Error("Response from server: " +
-                        response.statusText + ' (' +
-                        response.status + ')')
-                }
-                return response.text().then(text => {
-                    return { text, response }
-                })
-
-            })
-            .then(({text, response}) => {
-                let json, isJson
-                try {
-                    json = JSON.parse(text)
-                    isJson = true
-
-                } catch (e) {
-                    isJson = false
-                }
-                if (!isJson || !response.ok) {
-                    // If there was a problem, we want to
-                    // dispatch the error condition
-                    if (isJson) {
-                        // json.data = field level data
-                        dispatch(loginError(json.message, json.data))
-                    } else {
-                        dispatch(loginError(text))
-                    }
-                    callback(false)
-                } else {
-                    // save token
-                    localStorage.setItem('jsonwebtoken',json.token)
-                    // Dispatch the success action
-                    dispatch(() => {
-                        dispatch(receiveLogin(json))
-                    })
-                    callback(true) // take success action, like close login form
-                }
-            })
-            .catch(err => {
-                dispatch(loginError(err.message))
-                callback(false)
-            })
-    }
 }
 
