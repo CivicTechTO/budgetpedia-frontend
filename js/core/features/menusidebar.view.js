2787064671882609fae93b27dd37b04c08af9628
Henrik Bechmann <henrik@therefore.ca>
2017-11-27 22:04:27 -0500

    new view

---
+++ js/core/features/menusidebar.view.js
@@ -0,0 +1,6 @@
+'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+const React = require("react");
+class MenuSidebar extends React.Component {
+}
+exports.default = MenuSidebar;
--- ts/core/features/menusidebar.view.tsx
+++ ts/core/features/menusidebar.view.tsx
@@ -1,1 +1,13 @@
+// copyright (c) 2017 Henrik Bechmann, Toronto, MIT Licence
 // menusidebar.view.tsx
+
+'use strict'
+
+import * as React from 'react' // required by bundler
+
+class MenuSidebar extends React.Component<any, any> {
+
+
+}
+
+export default MenuSidebar
