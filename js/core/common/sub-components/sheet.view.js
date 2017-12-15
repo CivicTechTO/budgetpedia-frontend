55a9bbe06235df6ddd00a84257107e07940dd96d
Henrik Bechmann <henrik@therefore.ca>
2017-12-15 13:45:13 -0500

    convert sheet function to component

--- js/core/common/sub-components/sheet.view.js
+++ js/core/common/sub-components/sheet.view.js
@@ -2,7 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const React = require("react");
 const Paper_1 = require("material-ui/Paper");
-let SheetView = props => (React.createElement("div", { style: { backgroundColor: '#d9d9d9', padding: '16px' } },
-    React.createElement(Paper_1.default, { zDepth: 3 },
-        React.createElement("div", { style: { padding: '16px' } }, "Sheet View"))));
+let content = 'Sheet View';
+class SheetView extends React.Component {
+    render() {
+        return (React.createElement("div", { style: { backgroundColor: '#d9d9d9', padding: '16px' } },
+            React.createElement(Paper_1.default, { zDepth: 3 },
+                React.createElement("div", { style: { padding: '16px' } }, content))));
+    }
+}
 exports.default = SheetView;
--- ts/core/common/sub-components/sheet.view.tsx
+++ ts/core/common/sub-components/sheet.view.tsx
@@ -14,13 +14,20 @@ import Paper from 'material-ui/Paper'
 
 let content = 'Sheet View'
 
-let SheetView = props => (
-    <div style = {{backgroundColor:'#d9d9d9',padding: '16px'}}>
-        <Paper  zDepth = {3}>
-            <div style = {{padding:'16px'}}>
-                {content}
+class SheetView extends React.Component<any,any> {
+
+    render() {
+
+        return (
+            <div style = {{backgroundColor:'#d9d9d9',padding: '16px'}}>
+                <Paper  zDepth = {3}>
+                    <div style = {{padding:'16px'}}>
+                        {content}
+                    </div>
+                </Paper>
             </div>
-        </Paper>
-    </div>)
+        )
+    }
+}
 
 export default SheetView
