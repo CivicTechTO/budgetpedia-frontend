f966b74d947a6fa46970ef80a0152e64014737ef
Henrik Bechmann <henrik@therefore.ca>
2017-12-15 14:50:10 -0500

    get draft example working

--- js/core/common/sub-components/sheet.view.js
+++ js/core/common/sub-components/sheet.view.js
@@ -2,12 +2,45 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const React = require("react");
 const Paper_1 = require("material-ui/Paper");
-let content = 'Sheet View';
+const draft_js_1 = require("draft-js");
+const sampleMarkup = '<b>Bold text</b>, <i>Italic text</i><br/ ><br />' +
+    '<a href="http://www.facebook.com">Example link</a>';
+const blocksFromHTML = draft_js_1.convertFromHTML(sampleMarkup);
+console.log('blocksFromHTML', blocksFromHTML);
+const state = draft_js_1.ContentState.createFromBlockArray(blocksFromHTML.contentBlocks, blocksFromHTML.entityMap);
+function findLinkEntities(contentBlock, callback, contentState) {
+    contentBlock.findEntityRanges((character) => {
+        const entityKey = character.getEntity();
+        return (entityKey !== null &&
+            contentState.getEntity(entityKey).getType() === 'LINK');
+    }, callback);
+}
+const Link = (props) => {
+    const { url } = props.contentState.getEntity(props.entityKey).getData();
+    return (React.createElement("a", { href: url, target: '_blank' }, props.children));
+};
+const decorator = new draft_js_1.CompositeDecorator([
+    {
+        strategy: findLinkEntities,
+        component: Link,
+    },
+]);
 class SheetView extends React.Component {
+    constructor() {
+        super(...arguments);
+        this.state = {
+            editorState: draft_js_1.EditorState.createWithContent(state, decorator),
+        };
+        this.onEditorChange = (editorState) => this.setState({ editorState });
+    }
+    componentDidMount() {
+        console.log('sheet props', this.props);
+    }
     render() {
         return (React.createElement("div", { style: { backgroundColor: '#d9d9d9', padding: '16px' } },
             React.createElement(Paper_1.default, { zDepth: 3 },
-                React.createElement("div", { style: { padding: '16px' } }, content))));
+                React.createElement("div", { style: { padding: '16px' } },
+                    React.createElement(draft_js_1.Editor, { editorState: this.state.editorState, onChange: this.onEditorChange, readOnly: true })))));
     }
 }
 exports.default = SheetView;
--- ts/core/common/sub-components/sheet.view.tsx
+++ ts/core/common/sub-components/sheet.view.tsx
@@ -7,14 +7,68 @@ import * as React from 'react'
 
 import Paper from 'material-ui/Paper'
 
+import { Editor, EditorState, ContentState, convertFromHTML, convertFromRaw, CompositeDecorator } from 'draft-js'
+
 /*
     outer div = frameStyles
     inner dif = linerStyles
 */
 
-let content = 'Sheet View'
+const sampleMarkup =
+  '<b>Bold text</b>, <i>Italic text</i><br/ ><br />' +
+  '<a href="http://www.facebook.com">Example link</a>'
+
+const blocksFromHTML = convertFromHTML(sampleMarkup)
+console.log('blocksFromHTML',blocksFromHTML)
+const state = ContentState.createFromBlockArray(
+  blocksFromHTML.contentBlocks,
+  blocksFromHTML.entityMap
+)
+
+function findLinkEntities(contentBlock, callback, contentState) {
+contentBlock.findEntityRanges(
+  (character) => {
+    const entityKey = character.getEntity();
+    return (
+      entityKey !== null &&
+      contentState.getEntity(entityKey).getType() === 'LINK'
+    );
+  },
+  callback
+);
+}
+const Link = (props) => {
+    const {url} = props.contentState.getEntity(props.entityKey).getData();
+    return (
+      <a href={url} target = '_blank'>
+        {props.children}
+      </a>
+    );
+};
+
+const decorator = new CompositeDecorator([
+{
+  strategy: findLinkEntities,
+  component: Link,
+},
+// {
+//   strategy: findImageEntities,
+//   component: Image,
+// },
+])
+
 
 class SheetView extends React.Component<any,any> {
+
+    state = {
+      editorState: EditorState.createWithContent(state,decorator),
+    }
+
+    onEditorChange = (editorState) => this.setState({editorState});
+
+    componentDidMount() {
+        console.log('sheet props',this.props)
+    }
 
     render() {
 
@@ -22,7 +76,11 @@ class SheetView extends React.Component<any,any> {
             <div style = {{backgroundColor:'#d9d9d9',padding: '16px'}}>
                 <Paper  zDepth = {3}>
                     <div style = {{padding:'16px'}}>
-                        {content}
+                        <Editor 
+                            editorState = {this.state.editorState} 
+                            onChange = {this.onEditorChange}
+                            readOnly = {true}
+                        />
                     </div>
                 </Paper>
             </div>
