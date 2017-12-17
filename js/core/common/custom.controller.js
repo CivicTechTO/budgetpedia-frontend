6c1f0a67bd0da132a7f0e2270e4f9207198142e2
Henrik Bechmann <henrik@therefore.ca>
2017-12-17 06:59:01 -0500

    rebuild js dir

--- js/core/common/custom.controller.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-let CustomController = (props) => (React.createElement("div", null, "Custom"));
-exports.default = CustomController;
