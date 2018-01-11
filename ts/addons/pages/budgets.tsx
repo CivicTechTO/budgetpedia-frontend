5419f84231efe729c1348d89deffb0500aef4ae5
Henrik Bechmann <henrik@therefore.ca>
2018-01-11 15:09:46 -0500

    prune budgets page

--- ts/addons/pages/budgets.tsx
+++ ts/addons/pages/budgets.tsx
@@ -1,70 +1,12 @@
-// tribes.tsx
-// required by bundler
+// budgets.tsx
+
 import * as React from 'react'
-var { Component } = React
+
 import {Card, CardTitle, CardText} from 'material-ui/Card'
-let moment = require('moment')
 
-interface Phase {
-    index: number,
-    events:any[],
-    title:string,
-    subtitle?:string,
-}
+let Budgets = () => {
 
-interface BudgetEvent {
-    budget_event:string,
-    budget_event_code: string,
-    budget_type: string,
-    category: string,
-    location: string,
-    date:string,
-    notes:string,
-    phase:string,
-    public: string,
-}
-
-interface Roadmap {
-    events:any[],
-    phases:Phase[],
-}
-
-interface State {
-    roadmap:any // Roadmap
-}
-
-class Budgets extends Component<any, State> {
-
-    state = {
-        roadmap:null
-    }
-
-    componentDidMount() {
-
-        fetch('./db/repositories/toronto/roadmaps/government_events.json').then( response => {
-            if (response.ok) {
-                return response.json()
-            } else {
-                console.log('response error',response)
-            }
-        }).then( json => {
-            this.setState({
-                roadmap:json
-            })
-        }).catch( error => {
-            console.log('error',error)
-        })
-
-    }
-
-    // <h3>Rate supported budgets (waste, water, parking)</h3>
-    // <ul>
-    // </ul>
-    // <h3>Tax supported budgets (eveything else)</h3>
-    // <ul>
-    // </ul>
-
-    roadmapintro = <div>
+    let pagetitle = ( 
         <Card> 
             <CardTitle 
                 title = {"Budgets"}
@@ -73,7 +15,9 @@ class Budgets extends Component<any, State> {
             <CardTitle 
                 title = "2018"
             /> 
-        </Card>
+        </Card>)
+
+    let budget2018 = (
         <Card initiallyExpanded>
             <CardTitle 
                 actAsExpander={true}
@@ -123,7 +67,9 @@ class Budgets extends Component<any, State> {
             <CardTitle 
                 title = "2017"
             /> 
-        </Card>
+        </Card>)
+
+    let investing = (
         <Card initiallyExpanded >
             <CardTitle title = {'Toronto\'s "Investing in our future" consultations'} 
                 actAsExpander={true}
@@ -149,8 +95,10 @@ class Budgets extends Component<any, State> {
                 </ul>
                 <p>There is still a public survey available for input <a target="_blank" href="http://www.investinginto.ca/join-the-consultation/governance-survey">here</a> April 22 - May 14.</p>
             </CardText>
-        </Card>
-        <Card >
+        </Card>)
+
+   let budget2017 = (
+       <Card initiallyExpanded >
             <CardTitle 
                 actAsExpander={true}
                 showExpandableButton={true}
@@ -229,220 +177,17 @@ class Budgets extends Component<any, State> {
                             target="_blank" href="http://app.toronto.ca/tmmis/video.do?id=11852">video</a>)</li>
             </ul>
             </CardText>
-        </Card>
-        <hr style = {
-            {
-                borderWidth:"4px",
-                borderStyle:"outset",
-            }
-        }
-        />
-        <Card>
-            <CardTitle 
-                title = "2016"
-            /> 
-            <CardTitle 
-                title = "2016 sample decision roadmap pattern"
-            /> 
-            <CardText>
-            <div>Below is a summary of the program-by-program decision making process used for the Toronto 2016 budget, to provide some insight into the annual cycle.</div>
-            <div>(A program is a division or an agency)</div>
-            <div>The data was gathered through a combination of public sources and interviews with city staff.</div>
-            </CardText>
-        </Card>
-        </div>
+        </Card>)
 
-    phases:Phase[] = null
+    return <div>
 
-    prepareRoadmap = () => {
+        {pagetitle}
+        {budget2018}
+        {investing}
+        {budget2017}
 
-        if (!this.state.roadmap) return
-        if (this.phases) return
-        let roadmap = this.state.roadmap
-        let phases = roadmap.phases
-        let rawevents = roadmap.events as Object[]
-        let rawevent:any
+    </div>
 
-        for (rawevent of rawevents) {
-            phases[rawevent.phase].events.push(rawevent)
-        }
-
-        let phaselist:Phase[] = []
-        for (let phasename in phases) {
-            phaselist.push(phases[phasename])
-        }
-
-        phaselist = phaselist.sort((a,b) => {
-            return a.index - b.index
-        })
-
-        this.phases = phaselist
-
-    }
-
-    getEventClusterElement = (
-        eventcode, 
-        lookups, 
-        eventslist, 
-        phasetitle) => {
-
-        return <CardText
-            expandable = {true}
-            style = {{
-                border:"1px solid silver",
-                margin:"0 3px 8px 3px",
-                borderRadius:"8px",
-            }}
-            key = {eventcode}
-        > 
-            <div style={
-                {
-                    fontStyle:'italic',
-                    marginBottom:"8px"
-                }
-            }>
-
-                { 'Part of ' + phasetitle + ' phase: ' + lookups[eventcode] }
-
-            </div>
-
-            { eventslist }
-
-        </CardText>
-    }
-
-    getEventElement = (event:BudgetEvent,eventindex) => {
-
-        return <div
-            key = {eventindex}
-            style = {{
-                border:"1px dashed silver",
-                margin:"0 3px 8px 3px",
-                padding:"3px",
-                borderRadius:"8px",
-            }}
-        >
-
-            <div><em>Budget type:</em> { event.budget_type } </div>
-            <div><em>Description:</em> { event.budget_event } </div>
-            {event.date?<div><em>Date:</em> { moment(event.date,'YYYY-M-D').format('MMMM D, YYYY') }</div>:null }
-            {event.location?<div><em>Location:</em> { event.location } </div>:null}
-            {event.notes?<div><em>Notes:</em> { event.notes } </div>:null}
-            <div><em>Public:</em> { event.public } </div>
-
-        </div>
-
-    }
-
-    getPhaseContent = (events, phasetitle) => {
-
-        let lookups = this.state.roadmap.lookups
-        let eventcode = null
-        let eventClusterElements = []
-        let eventClusterElement = null
-        let eventslist
-        let event:BudgetEvent = null
-
-        for (let eventindex in events) {
-
-            event = events[eventindex]
-            if (event.budget_event_code !== eventcode) {
-                if (eventcode) {
-                    eventClusterElement = this.getEventClusterElement(
-                        eventcode,lookups,eventslist,phasetitle)
-                    eventClusterElements.push(eventClusterElement)
-                }
-                eventcode = event.budget_event_code
-                eventslist = []
-            }
-            let eventElement = this.getEventElement(event, eventindex)
-            eventslist.push(eventElement)
-
-        }
-
-        // tail settlement
-        if (eventcode) {
-
-            eventClusterElement = this.getEventClusterElement(
-                eventcode,lookups,eventslist,phasetitle)
-            eventClusterElements.push(eventClusterElement)
-
-        }
-
-        return eventClusterElements
-
-    }
-
-    getRoadmap = () => {
-
-        if (!this.phases) return null
-
-        let phasesinput = this.phases
-        let phases = phasesinput.map((phase,index) => {
-
-            let phasecontent = this.getPhaseContent(phase.events, phase.title)
-            let [startdate, enddate] = this.getDateRange(phase.events)
-            let phaseElement = <Card
-                    key = {phase.index}
-                >
-                <CardTitle 
-                    actAsExpander={true}
-                    showExpandableButton={true}
-                    title = { phase.title }
-                    subtitle = {phase.subtitle + ' from ' + 
-                        moment(startdate,'YYYY-MM-DD').format('MMMM D, YYYY') + ' to ' + 
-                        moment(enddate,'YYYY-MM-DD').format('MMMM D, YYYY')
-                    }
-                />
-
-                { phasecontent }
-
-            </Card>
-
-            return phaseElement
-
-        })
-
-        return phases
-
-    }
-
-    getDateRange = (events) => {
-
-        let startdate = null
-        let enddate = null
-        for (let event of events) {
-
-            let eventdate = moment(event.date,'YYYY-M-D').format('YYYY-MM-DD')
-
-            if (!startdate || startdate > eventdate) {
-                startdate = eventdate
-            }
-
-            if (!enddate || enddate < eventdate) {
-                enddate = eventdate
-            }
-
-        }
-
-        return [startdate,enddate]
-
-    }
-
-    render() {
-
-        this.prepareRoadmap()
-        let roadmap = this.getRoadmap()
-
-        return <div>
-
-            { this.roadmapintro }
-
-            { roadmap }
-
-        </div>
-
-    }
 }
 
 export default Budgets
